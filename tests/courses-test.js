8e38906adbe0e62ff996ad96f7f26cc5738d1f0f
Julia Zeckl <20083071@wit.ie>
2018-11-05 09:51:45 +0000

    updated before hooks

--- tests/courses-test.js
+++ tests/courses-test.js
@@ -30,13 +30,86 @@ db.once('open', function () {
 
 /*var cat = null;
 var invoice = null;*/
-var userID="5bcc7c1a078bcd01249cec60";
-var courseID1="5bcc88768574d7028aac9502";
-var courseID2="5bcc8896a5014c029002dc0d";
+var userID=null;
+var courseID1=null;
+var courseID2=null;
 var courseID3=null;
 
 describe('Courses', function (){
+    before(function(done){
+        User.deleteMany({},function(){
+            done();
+        });
+    });
+    before(function(done){
+        Course.deleteMany({},function(){
+            done();
+        });
+    });
+    before(function(done){
+        var newuser= new User();
+        newuser.firstname="Test";
+        newuser.lastname="User1";
 
+        newuser.save(function(err) {
+            if (err)
+                console.log({message:"User not Added",errmsg:err});
+            else{
+                console.log({ message: 'User Added!',userID:newuser._id});
+                userID=newuser._id.toString();
+                done();
+            }
+        });
+    });
+
+    before(function(done){
+
+        var course= new Course();
+        course.coursetype="morse";
+        course.userId=userID;
+        course.score=5;
+        course.coursecontent=[
+            7,
+            13,
+            21
+        ];
+
+        course.save(function(err) {
+            if (err)
+                console.log({message:"Course not Added",errmsg:err});
+            else
+            {
+                console.log({ message: 'Course Added!',courseID:course._id});
+                courseID1=course._id.toString();
+                done();
+            }
+        });
+    });
+    before(function(done){
+
+        var course2= new Course();
+        course2.coursetype="morse";
+        course2.userId=userID;
+        course2.score=7;
+        course2.coursecontent=[
+            22,
+            1,
+            19,
+            23,
+            7
+        ];
+
+        course2.save(function(err) {
+            if (err)
+                console.log({message:"Course not Added",errmsg:err});
+            else
+            {
+                console.log({ message: 'Course Added!',courseID:course2._id});
+                courseID2=course2._id.toString();
+                done();
+            }
+        });
+    });
 
     describe('GET /courses',  () => {
         it('should return all the courses in an array', function(done) {
